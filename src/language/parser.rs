850c2ee44f39617778d4611f8d5a83a3fee4c484
Michael Goodale <michael.goodale@mail.mcgill.ca>
2025-02-17 15:02:54 +0100

    Added basic parsing

--- src/language/parser.rs
+++ src/language/parser.rs
@@ -5,22 +5,19 @@ use crate::{
 use chumsky::extra;
 use chumsky::prelude::*;
 
+use super::BinOp;
+
 type ExtraType<'a> = extra::Full<Simple<'a, char>, extra::SimpleState<ExprPool>, ()>;
 
 fn parser<'a>() -> impl Parser<'a, &'a str, ExprRef, ExtraType<'a>> {
-    let actor = just("a")
-        .ignore_then(text::int::<&str, ExtraType>(10))
-        .map_with(|num, e| {
-            e.state()
-                .add(Expr::Entity(Entity::Actor(num.parse().unwrap())))
-        })
-        .padded();
-
-    let event = just("e")
-        .ignore_then(text::int::<&str, ExtraType>(10))
-        .map_with(|num, e| {
-            e.state()
-                .add(Expr::Entity(Entity::Actor(num.parse().unwrap())))
+    let actor_or_event = one_of("ae")
+        .then(text::int::<&str, ExtraType>(10))
+        .map_with(|(c, num), e| {
+            e.state().add(Expr::Entity(match c {
+                'a' => Entity::Actor(num.parse().unwrap()),
+                'e' => Entity::Event(num.parse().unwrap()),
+                _ => panic!("Unreachable because of one_of"),
+            }))
         })
         .padded();
 
@@ -31,11 +28,9 @@ fn parser<'a>() -> impl Parser<'a, &'a str, ExprRef, ExtraType<'a>> {
     .map_with(|c, e| e.state().add(Expr::Constant(c)))
     .padded();
 
-    let constants = choice((
-        text::ascii::keyword::<_, _, ExtraType>("everyone").to(Constant::Everyone),
-        text::ascii::keyword::<_, _, ExtraType>("everyevent").to(Constant::EveryEvent),
-        text::ascii::keyword::<_, _, ExtraType>("True").to(Constant::Tautology),
-        text::ascii::keyword::<_, _, ExtraType>("False").to(Constant::Contradiction),
+    let sets = choice((
+        just("everyone").to(Constant::Everyone),
+        just("everyevent").to(Constant::EveryEvent),
         just("p")
             .ignore_then(text::int::<&str, ExtraType>(10))
             .map(|p| Constant::Property(p.parse().unwrap())),
@@ -43,15 +38,29 @@ fn parser<'a>() -> impl Parser<'a, &'a str, ExprRef, ExtraType<'a>> {
     .map_with(|c, e| e.state().add(Expr::Constant(c)))
     .padded();
 
+    let entity = actor_or_event.padded();
+
     let truth_value = recursive(|expr| {
         let atom = true_or_false
             .or(expr.delimited_by(just('('), just(')')))
             .padded();
-        let neg = just("~").repeated().foldr_with(atom, |_, b, e| {
-            let pool = e.state();
-            pool.add(Expr::Unary(MonOp::Not, b))
-        });
-        neg
+
+        let bin_op = choice((
+            just("AgentOf").to(BinOp::AgentOf),
+            just("PatientOf").to(BinOp::PatientOf),
+        ))
+        .then_ignore(just('('))
+        .then(entity)
+        .then_ignore(just(','))
+        .then(entity)
+        .then_ignore(just(')'))
+        .map_with(|((binop, actor), event), e| e.state().add(Expr::Binary(binop, actor, event)));
+
+        let neg = just("~")
+            .repeated()
+            .foldr_with(atom, |_, b, e| e.state().add(Expr::Unary(MonOp::Not, b)));
+
+        neg.or(bin_op)
     });
 
     truth_value.then_ignore(end())
@@ -66,10 +75,6 @@ mod tests {
 
     #[test]
     fn parse_test() -> anyhow::Result<()> {
-        let mut pool = extra::SimpleState(ExprPool::default());
-        let parse = parser().parse_with_state("~~~False", &mut pool).unwrap();
-        println!("{parse:?}");
-        println!("{:?}", pool.0);
         let mut variables = VariableBuffer(vec![]);
         let simple_scenario = Scenario {
             actors: vec![0, 1],
@@ -86,6 +91,17 @@ mod tests {
             properties: HashMap::default(),
         };
 
+        let mut pool = extra::SimpleState(ExprPool::default());
+        let parse = parser().parse_with_state("~~~False", &mut pool).unwrap();
+        assert_eq!(
+            pool.0.interp(parse, &simple_scenario, &mut variables),
+            LanguageResult::Bool(true)
+        );
+
+        let mut pool = extra::SimpleState(ExprPool::default());
+        let parse = parser()
+            .parse_with_state("AgentOf(a1,  e1)", &mut pool)
+            .unwrap();
         assert_eq!(
             pool.0.interp(parse, &simple_scenario, &mut variables),
             LanguageResult::Bool(true)
